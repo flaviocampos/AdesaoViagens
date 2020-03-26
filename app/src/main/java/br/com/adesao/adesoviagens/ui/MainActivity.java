aca0f12baa286ebe08a38ff417c314028f8afa43
Flávio Campos - MM <flaviorcampos15@gmail.com>
2020-03-25 21:06:43 -0300

    Versāo Incial

---
+++ app/src/main/java/br/com/adesao/adesoviagens/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.adesao.adesoviagens.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.adesao.adesoviagens.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
